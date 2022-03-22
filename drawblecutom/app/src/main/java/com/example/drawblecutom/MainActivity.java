612534eed239509d1956e2461c708d5385e801a5
quanganhdev <quanganhle.dev@gmai;.com>
2022-03-22 21:10:50 +0700

    android

--- drawblecutom/app/src/main/java/com/example/drawblecutom/MainActivity.java
+++ drawblecutom/app/src/main/java/com/example/drawblecutom/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.drawblecutom;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.ClipDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView img;
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        img = (ImageView) findViewById(R.id.imageView2);
+        btn = (Button) findViewById(R.id.button2);
+        img.setImageLevel(1000);
+        ClipDrawable clipDrawable = (ClipDrawable) img.getDrawable();
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int curent= clipDrawable.getLevel();
+                if(curent>=10000){
+                    curent=0;
+                }
+       img.setImageLevel(curent+1000);
+            }
+        });
     }
 }

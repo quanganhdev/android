3b0c2552ae5584216d0ab65cc4fddd560a0b3869
quanganhdev <quanganhle.dev@gmai;.com>
2022-03-29 15:39:55 +0700

    h

--- dialog/app/src/main/java/com/example/dialog/MainActivity.java
+++ dialog/app/src/main/java/com/example/dialog/MainActivity.java
@@ -9,13 +9,16 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     ListView lvname;
     ArrayList <String> array;
-        ArrayAdapter arrayAdapter;
+    ArrayAdapter arrayAdapter;
+    TextView dangnhap;
+
 
 
     @Override
